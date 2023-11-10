5c2977fa4dc43a0b86bbd5c1d8ec042dd8818582
Lika <114234069+Basilika-draconum@users.noreply.github.com>
2023-11-10 16:35:26 +0200

    Update Slider.tsx

--- components/Slider/Slider.tsx
+++ components/Slider/Slider.tsx
@@ -63,73 +63,30 @@ const Slider = () => {
                   <div>
                     <div className="flex flex-col-reverse gap-6 mb-12 mt-2 tablet:flex-col desktop:flex-row desktop:gap-2">
                       <ul className="uppercase flex flex-col gap-4 text-xl font-extralight leading-[17px] tablet:text-[22px] desktop:text-[28px] desktop:gap-6 desktop:leading-6 desktop:cursor-pointer">
-                        {slide.title === "ATVs Traveling" ? (
-                          <li className="font-medium isActive ml-2 desktop:ml-3">
-                            ATVs Traveling
-                          </li>
-                        ) : (
-                          <li
-                            className="text-white/50"
-                            onClick={() => handleSlideClick(0)}
-                          >
-                            ATVs Traveling
-                          </li>
-                        )}
-                        {slide.title === "Rock climbing" ? (
-                          <li className="font-medium isActive ml-2 desktop:ml-3">
-                            Rock climbing
-                          </li>
-                        ) : (
-                          <li
-                            className="text-white/50"
-                            onClick={() => handleSlideClick(1)}
-                          >
-                            Rock climbing
-                          </li>
-                        )}
-                        {slide.title === "Hot air ballooning" ? (
-                          <li className="font-medium isActive ml-2 desktop:ml-3">
-                            Hot air ballooning
-                          </li>
-                        ) : (
-                          <li
-                            className="text-white/50"
-                            onClick={() => handleSlideClick(2)}
-                          >
-                            Hot air ballooning
-                          </li>
-                        )}
-                        {slide.title === "Skydiving" ? (
-                          <li className="font-medium isActive ml-2 desktop:ml-3">
-                            Skydiving
-                          </li>
-                        ) : (
-                          <li
-                            className="text-white/50 "
-                            onClick={() => handleSlideClick(3)}
-                          >
-                            Skydiving
-                          </li>
-                        )}
-
-                        {slide.title === "Rafting" ? (
-                          <li className="font-medium isActive ml-2 desktop:ml-3">
-                            Rafting
-                          </li>
-                        ) : (
-                          <li
-                            className="text-white/50"
-                            onClick={() => handleSlideClick(4)}
-                          >
-                            Rafting
-                          </li>
+                        {slides.map((item, index) =>
+                          slide.title === item.title ? (
+                            <li
+                              key={item.id}
+                              className="font-medium isActive ml-2 desktop:ml-3"
+                            >
+                              {item.title}
+                            </li>
+                          ) : (
+                            <li
+                              className="text-white/50"
+                              key={item.id}
+                              onClick={() => handleSlideClick(index)}
+                            >
+                              {item.title}
+                            </li>
+                          )
                         )}
                       </ul>
-                      <p className="text-text text-[12px] leading-6 tracking-[2.4px] text-right tablet:text-left desktop:w-[107px]">
+                      <p className="text-text text-[12px] leading-6 tracking-[2.4px] text-right tablet:text-left desktop:w-[147px]">
                         {slide.slogan}
                       </p>
                     </div>
-                    <p className="text-text leading-5 tablet:text-[13px] tablet:text-justify ">
+                    <p className="text-text leading-5 tablet:text-[13px] tablet:text-justify desktop:w-[293px] desktop:text-[18px] desktop:leading-6 desktop:ml-52">
                       {slide.description}
                     </p>
                   </div>
